8a9f001ed94720bf37521f8b010acf972902b5da
und3d <cckokanour@gmail.com>
2025-05-05 18:40:06 -0700

    Added Project File

---
+++ New 2D Siege/Assets/Scripts/Game_States/EndOfRoundState.cs
@@ -0,0 +1,17 @@
+using PurrNet.StateMachine;
+using UnityEngine;
+
+public class EndOfRoundState : StateNode
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5b4a28e0235b2476e407a85abec3df25235c8424
und3d <cckokanour@gmail.com>
2025-09-07 00:42:57 -0700

    0.0.10.3 Tox Gadget Functionality
    - Updated Unity version to 6.2
    - Added Tox gadget functionality
    - Tox Cloud goes through walls
    - Lasts 10 seconds
    - Does 20 dmg/second
    - Clouds stack damage

--- New 2D Siege/Assets/Scripts/Gadgets/Throwables/ToxGadget/ToxGadget.cs
+++ New 2D Siege/Assets/Scripts/Gadgets/Throwables/ToxGadget/ToxGadget.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Collections.Generic;
+using PurrNet;
 using UnityEngine;
 
 public class ToxGadget : ThrowableGadget
 {
+    [SerializeField] private SpriteRenderer gadgetSprite;
+    [SerializeField] private SpriteRenderer toxCloud;
+    [SerializeField] private CircleCollider2D toxCloudCollider;
+    [SerializeField] private float timeBetweenTicks = 0.5f;
+    [SerializeField] private float toxCloudDuration = 15f;
+    [SerializeField] private int damagePerTick = 10;
+    
+    public List<GameObject> playersInToxCloud;
+    private float lastToxTick;
+    private float timeOfActivation;
+    
     protected override void Update()
     {
         if (!isOwner)
@@ -11,6 +24,21 @@ public class ToxGadget : ThrowableGadget
             return;
         if (!gameController)
             return;
+        
+        if (toxCloud.enabled)
+        {
+            if (Time.unscaledTime - timeOfActivation > toxCloudDuration)
+                Destroy(gameObject);
+            
+            if (!(Time.unscaledTime - lastToxTick > timeBetweenTicks)) 
+                return;
+            
+            DamagePlayers(playersInToxCloud);
+            
+            lastToxTick = Time.unscaledTime;
+
+            return;
+        }
         
         if (isOwner && !(Time.unscaledTime < timeAtPlacement + timeBeforePickup) && canBePickedUp)
         {
@@ -41,11 +69,11 @@ public class ToxGadget : ThrowableGadget
         
         if (roundView.IsGadgetPickupTextActive())
         {
-            Debug.Log($"isGadgetInteractTextActive: {roundView.IsGadgetPickupTextActive()}", this);
+            //Debug.Log($"isGadgetInteractTextActive: {roundView.IsGadgetPickupTextActive()}", this);
             return;
         }
         
-        Debug.Log($"isGadgetInteractTextActive: {roundView.IsGadgetPickupTextActive()}", this);
+        //Debug.Log($"isGadgetInteractTextActive: {roundView.IsGadgetPickupTextActive()}", this);
         
         if (Input.GetKeyDown(KeyCode.F))
         {
@@ -62,10 +90,46 @@ public class ToxGadget : ThrowableGadget
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player") || other.gameObject == playerObject)
+            return;
+        
+        playersInToxCloud.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!playersInToxCloud.Contains(other.gameObject))
+            return;
+        playersInToxCloud.Remove(other.gameObject);
+    }
+
+    [ObserversRpc]
     protected override void ActivateGadget()
     {
+        FreezeGadget(true);
+        gadgetSprite.enabled = false;
+        toxCloud.enabled = true;
+        toxCloudCollider.enabled = true;
         
-        
-        base.ActivateGadget();
+        lastToxTick = Time.unscaledTime;
+        timeOfActivation = Time.unscaledTime;
+    }
+
+    [ServerRpc]
+    private void DamagePlayers(List<GameObject> playerList)
+    {
+        foreach (var player in playerList)
+        {
+            if (!player.TryGetComponent(out PlayerHealth playerHealth))
+            {
+                Debug.LogError($"Player {player.name} does not have a PlayerHealth component!", this);
+                continue;
+            }
+
+            if (owner != null) playerHealth.ChangeHealth(-damagePerTick, owner.Value);
+            Debug.Log($"Player {player.name} has been hit by toxic cloud for {damagePerTick} damage.");
+        }
     }
 }
